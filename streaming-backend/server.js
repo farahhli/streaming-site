f4c401ca857000990a4d14ade95a6068f82fa5ba
Farahhli <farahhli86@gmail.com>
2024-09-21 14:17:42 +0100

    Fix Heroku port binding

--- streaming-backend/server.js
+++ streaming-backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const port = process.env.PORT || 3000;  // Use the port provided by Heroku or default to 3000 locally
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
